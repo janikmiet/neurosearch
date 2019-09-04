0acce3ed59bf2d5abf85e1a77cc803bf943deaa9
janikmiet <jani.k.miettinen@gmail.com>
2019-09-04 07:38:29 +0300

    added zero config

--- run.R
+++ run.R
@@ -1,2 +1,3 @@
 # run.R
 if(Sys.info()[["nodename"]] == "solu") system("cp neurosearch.Rmd /usr/local/apps/shiny-server/neurosearch")
+if(Sys.info()[["nodename"]] == "zero") system("cp neurosearch.Rmd ~/ShinyApps/neurosearch")
